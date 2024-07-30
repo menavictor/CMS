84b84182d15dabed28ed8d47fbbd49c37010f2da
Mina Victor <a.mvictor@dev.thiqah.sa>
2024-07-30 14:13:35 +0300

    Update Data Type

--- CMS.Domain/Entities/Business/GroupLevel.cs
+++ CMS.Domain/Entities/Business/GroupLevel.cs
@@ -1,7 +1,6 @@
 ﻿using CMS.Domain.Entities.Base;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace CMS.Domain.Entities.Business
 {
@@ -11,10 +10,7 @@ namespace CMS.Domain.Entities.Business
         public string Description { get; set; }
         public Guid? ParentId { get; set; }
         public GroupLevel Parent { get; set; }
-
-        public virtual ICollection<EmployeeGroupLevel> EmployeeGroupLevels { get; set; } = new Collection<EmployeeGroupLevel>();
-
-
+        public ICollection<EmployeeGroupLevel> EmployeeGroupLevels { get; set; } = new HashSet<EmployeeGroupLevel>();
 
     }
 }
